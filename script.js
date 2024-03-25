d8ee2db22991c2ca7689beebc968462f8cf36aa6
aminbabu12 <badrulaminbabu@gmail.com>
2024-03-25 13:03:42 -0400

    navber  has responsived

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('header');
+const navlist = document.querySelector('.navlist');
+const menu = document.querySelector('.fa-bars');
 
 window.onscroll = ()=>{
   if(window.scrollY > 100){
@@ -7,3 +9,8 @@ window.onscroll = ()=>{
     header.classList.remove('scroll');
   }
 }
+
+menu.addEventListener('click',()=>{
+  navlist.classList.toggle('show');
+  menu.classList.toggle('fa-xmark');
+})
